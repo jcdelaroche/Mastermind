420782da72c814e6a12d774b5f882941f33f9356
Erym974 <remyrichard974@live.fr>
2023-05-11 18:58:55 +0200

    Merging Server and Front

---
+++ front/src/Constant/Constant.js
@@ -0,0 +1,5 @@
+export const GameState = {
+    PLAYING: 'PLAYING',
+    LOST: 'LOST',
+    WON: 'WON',
+};
--- src/Constant/Constant.js
+++
@@ -1,5 +0,0 @@
-export const GameState = {
-    PLAYING: 'PLAYING',
-    LOST: 'LOST',
-    WON: 'WON',
-};
